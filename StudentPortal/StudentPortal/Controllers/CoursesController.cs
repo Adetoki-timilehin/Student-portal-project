ebc064c3b8b298892bec847d152dcde146da1ce8
Adetoki_Timilehin <tadetoki@axxess.com>
2023-05-07 22:52:06 +0100

    changes

---
+++ StudentPortal/StudentPortal/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentPortal.Models;
+
+namespace StudentPortal.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void RegisterCourses(Courses model)
+        {
+            var CourseDetails = new List<string> { model.CourseCode, model.CourseUnits };
+            model.CourseList.Add(model.CourseName, CourseDetails);
+        }
+    }
+}
---
+++ StudentPortal/StudentPortal/Models/User.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Web.Mvc.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Security;
+using System.Web;
+
+namespace StudentPortal.Models
+{
+    public class User
+    {
+        private string email;
+        public bool IsStudent;
+
+        [Required]
+        [EmailAddress]
+        public string Email {
+            get { return email; }
+            set {
+                if (!IsValidEmail(value))
+                {
+                    throw new ArgumentException("Invaild Email");
+                }
+                else
+                {
+                    email = value;
+                }
+            } }
+
+
+        public int MatricNo { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public SecureString Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mailAddress = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
